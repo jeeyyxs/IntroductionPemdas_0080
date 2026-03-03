ebd01bc0861003893a7423d8e87caec6d73576e6
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-03-03 15:33:11 +0700

    2

--- praktikum.cpp
+++ praktikum.cpp
@@ -3,3 +3,10 @@
 using namespace std;
 //Deklarasi Global
     float p,l;
+//Implementasi Prosedur & Fungsi
+void Input(){
+    cout << "Masukkan Panjang : " ;
+    cin >> p;
+    cout << "Masukkan Lebar : " ;
+    cin >> l;
+}
