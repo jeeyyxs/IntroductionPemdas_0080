cbc94cf17885ae8a327b1cbc4b7e2f5955c65c9c
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-03-03 15:40:02 +0700

    6

---
+++ praktikum2.cpp
@@ -0,0 +1,11 @@
+//Library
+#include<iostream>
+using namespace std;
+//Deklarasi GLobal
+//Implementasi Prosedur & Fungsi
+void Input(){
+    cout << "Masukkan Panjang : " ;
+    cin >> p;
+    cout << "Masukkan Lebar : " ;
+    cin >> l;
+}
